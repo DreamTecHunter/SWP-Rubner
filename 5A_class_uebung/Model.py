5fb48db02d4feba555a5a69025db3fcae915ad87
DreamTecHunter <tobias.laser@ymail.com>
2021-12-08 17:50:40 +0100

    first add of 5A_class_uebung

---
+++ 5A_class_uebung/Model.py
@@ -0,0 +1,114 @@
+import collections
+import copy
+from enum import Enum
+
+
+class Sex(Enum):
+    NONE_SPECIFIED = "none-specified"
+    M = "male"
+    F = "female"
+    pass
+
+
+class Department(Enum):
+    NONE_SPECIFIED = "none-specified"
+    HR_M = "Human-Resource-Management"
+    C_M = "Company-Management"
+    P = "Production"
+    pass
+
+
+class Company:
+    def __init__(self, workers: list = None, group_leaders: list = None):
+        self.workers = workers
+        self.group_leaders = group_leaders
+        pass
+
+    def amount_of_worker(self):
+        return self.workers is not None if len(
+            self.workers) else 0  # if self.worker is not None => False => return len(self.worker) ?
+
+    def amount_of_group_leader(self):
+        return self.group_leaders is not None if len(
+            self.group_leaders) else 0  # if self.group_leader is not None => False => return len(self.group_leader) ?
+
+    def amount_of_departments(self):
+        return len(self.find_departments())
+
+    def find_departments(self):
+        return self.sub_find_departments(self.workers) + self.sub_find_departments(self.group_leaders)
+
+    def sub_find_departments(self, people):
+        departments: list = []
+        if people is not None:
+            for person in people:
+                is_in_list = False
+                if len(departments) is not 0:
+                    for d in departments:
+                        if person.department is d:
+                            is_in_list = True
+                            break
+                if is_in_list is False:
+                    departments.append(person.department)
+        return collections.Counter(departments)
+
+    def find_amount_of_participates_per_departments(self):
+        temp_worker = self.group_leaders + self.workers;
+        temp_map: map
+        for d in self.find_departments():
+            temp_map.put(d, 0)
+        for worker in temp_worker:
+            temp_map.put(worker.department, temp_map.get(temp_worker) + 1)
+        return temp_map
+
+    def find_biggest_department(self):
+        pass
+
+pass
+
+
+class Person:
+    def __init__(self, lastname: str = "", firstname: str = "", age: int = 0, sex: Sex = Sex.NONE_SPECIFIED):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.age = age
+        self.sex = sex
+        pass
+
+    def to_string(self):
+        return "name: " + self.lastname + " " + self.firstname + "\nage\t: " + str(
+            self.age) + "\nsex\t: " + self.sex.value
+
+    pass
+
+
+class Worker(Person):
+    def __init__(self, lastname: str = "", firstname: str = "", age: int = 0, sex: Sex = Sex.NONE_SPECIFIED,
+                 department: Department = Department.NONE_SPECIFIED):
+        super(Worker, self).__init__(lastname, firstname, age, sex)
+        self.department = department
+        pass
+
+    def to_string(self):
+        return super(Worker, self).to_string() + "\ndepartment: " + self.department.value
+
+    pass
+
+
+class GroupLeader(Worker):
+    def __init__(self, lastname: str = "", firstname: str = "", age: int = 0, sex: Sex = Sex.NONE_SPECIFIED,
+                 department: Department = Department.NONE_SPECIFIED, subordinates: list = None):
+        super(GroupLeader, self).__init__(lastname, firstname, age, sex, department)
+        self.subordinates = subordinates
+        pass
+
+    def to_string(self):
+        content = super(GroupLeader, self).to_string() + "\nSubordinates: "
+        if self.subordinates is not None:
+            for s in self.subordinates:
+                content = content + "\n\t" + s.lastname + " " + s.firstname
+        else:
+            content = content + "\n\tNo subordinates"
+        return content
+
+    pass
---
+++ 5A_class_uebung/main.py
@@ -0,0 +1,39 @@
+import copy
+
+from Model import Sex, Department, Company, Person, Worker, GroupLeader
+
+if __name__ == '__main__':
+    empty_person = Person()
+    person = Person("test", "subject", 0, Sex.M)
+    empty_worker = Worker()
+    worker = Worker("Kefer", "Thomas", 19, Sex.M, Department.P)
+    subordinates = [person, worker]
+    worker.to_string()
+    empty_group_leader = GroupLeader();
+    group_leader = GroupLeader("Laser", "Tobias", 18, Sex.M, Department.P, subordinates)
+
+    people: list = [empty_person, person]
+    workers: list = [empty_worker, worker]
+    group_leaders: list = [empty_group_leader, group_leader]
+    all_people: list = people + workers + group_leaders
+
+    for temp_person in all_people:
+        print("\n" + temp_person.to_string())
+
+    print()
+    company = Company(workers, group_leaders)
+    # print("Amount of worker\t: " + company.amount_of_worker())
+    # print("Amount of group-leaders\t: " + company.amount_of_group_leader())
+    print("Amount of departments\t: " + str(company.amount_of_departments()))
+    print("departments\t: ")
+    for dep in company.find_departments():
+        print("\t" + dep.value)
+    """
+    for temp_map in company.find_amount_of_participates_per_departments():
+        print(temp_map)
+    """
+
+    """
+    #Geschlechterverteilung
+    """
+
